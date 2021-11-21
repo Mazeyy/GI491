c555675612bb73877155d1fc2ab8fc216e2cc49e
Minikorin <nathamon666@gmail.com>
2021-11-22 01:12:33 +0700

    Add interface and setting system
    setting window music slide bar still not available. pls set setting window layer

--- GameDesign_Game/Assets/Mainmenu.cs
+++ GameDesign_Game/Assets/Mainmenu.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Mainmenu : MonoBehaviour
 {
+    public GameObject SettingWindow;
+    public GameObject MusicImage;
+    public GameObject EffectImage;
+    public GameObject SaveBtn;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,5 +20,21 @@ public class Mainmenu : MonoBehaviour
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void Setting()
+    {
+        SettingWindow.SetActive(true);
+        MusicImage.SetActive(true);
+        EffectImage.SetActive(true);
+        SaveBtn.SetActive(true);
+    }
+    public void Save()
+    {
+        SettingWindow.SetActive(false);
+        MusicImage.SetActive(false);
+        EffectImage.SetActive(false);
+        SaveBtn.SetActive(false);
+        
     }
 }
